d575a83fd7160fba0c8e999b7fcf4000565fb5e4
那个谁 <lhh131114@outlook.com>
2017-09-12 13:39:08 +0800

    李辉洪

--- app/src/main/java/com/example/mrz/newproject/controller/activity/LoginActivity.java
+++ app/src/main/java/com/example/mrz/newproject/controller/activity/LoginActivity.java
@@ -7,10 +7,9 @@ import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -155,6 +153,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     }else{
                         Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                         startActivity(intent);
+                        finish();
                     }
 
 
