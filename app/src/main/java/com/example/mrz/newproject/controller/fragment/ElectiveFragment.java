2150133702ef4909ddc3ba8eb96445946bfb42a9
cqcet_三猿 <1261161486@qq.com>
2017-09-13 14:49:32 +0800

    更新 ElectiveFragment.java

--- app/src/main/java/com/example/mrz/newproject/controller/fragment/ElectiveFragment.java
+++ app/src/main/java/com/example/mrz/newproject/controller/fragment/ElectiveFragment.java
@@ -42,11 +42,17 @@ public class ElectiveFragment extends Fragment {
     private final int GET_SELECT_SUCCESED = 0X11111;
     private final int GET_SELECT_FAILED = 0X11110;
 
-
-    public ElectiveFragment(Context context, String url) {
-        this.context = context;
-        this.url = url;
-    }
+    private Context context;  
+    private String url;  
+  
+    //Fragment新要求这样些
+    public static ElectiveFragment newInstance(String url) {  
+        ElectiveFragment newFragment = new ElectiveFragment();  
+        Bundle bundle = new Bundle();  
+        bundle.putString("url", url);  
+        newFragment.setArguments(bundle);  
+        return newFragment;  
+    }  
 
     //有可选课时显示的条目
     @BindView(R.id.elective_rv_class)
@@ -69,6 +75,11 @@ public class ElectiveFragment extends Fragment {
         if(view == null){
             view = inflater.inflate(R.layout.fragment_elective,container,false);
             ButterKnife.bind(this, view);
+            this.context = getActivity();
+            Bundle args = getArguments();  
+            if (args != null) {  
+                this.url = args.getString("url");  
+            }  
         }
 
 
